e93eff691f6643ce3859bd34ccfbaa5757c8f123
Mathias Brønd Sørensen <mathias.b.sorensen@gmail.com>
2020-02-01 20:50:10 +0100

    Working MVP - will predict dog race and print to console of webapp

--- app/run.py
+++ app/run.py
@@ -1,5 +1,6 @@
 import json
 import plotly
+import numpy as np
 import pandas as pd
 import os , io , sys
 
@@ -8,7 +9,58 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar, Violin, Box
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+from io import BytesIO
+import base64
+#from keras import backend as K
+from keras.applications.resnet50 import ResNet50, decode_predictions
+from keras.applications.resnet50 import preprocess_input as rn50_preprocess_input
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.applications.inception_resnet_v2 import preprocess_input as irnv2_preproccess_input
+from keras.utils import np_utils
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint  
+from keras.preprocessing import image                  
+from tqdm import tqdm
+from sklearn.datasets import load_files
+from glob import glob
+from PIL import ImageFile, Image                           
+ImageFile.LOAD_TRUNCATED_IMAGES = True    
+#K.clear_session()
+#ResNet50_model = ResNet50(weights='imagenet')
+import tensorflow as tf
 
+# def load_model():
+	# global model
+	# model = ResNet50(weights="imagenet")
+            # # this is key : save the graph after loading the model
+	# global graph
+	# graph = tf.get_default_graph()
+# load_model()
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def img_to_tensor(img):
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)   
+    
+def ResNet50_predict_labels(file):
+    # returns prediction vector for image located at img_path
+    img = rn50_preprocess_input(img_to_tensor(file))
+    return np.argmax(ResNet50_model.predict(img))
+
+def dog_detector(file):
+    prediction = ResNet50_predict_labels(file)
+    return ((prediction <= 268) & (prediction >= 151)) 
 
 app = Flask(__name__)
 
@@ -111,28 +163,68 @@ def index():
     # render web page with plotly graphs
     return render_template('master.html')#, ids=ids, graphJSON=graphJSON)
 
-def mask_image():
-	# print(request.files , file=sys.stderr)
-	file = request.files['image'].read() ## byte file
-	npimg = np.fromstring(file, np.uint8)
-	img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
-	######### Do preprocessing here ################
-	# img[img > 150] = 0
-	## any random stuff do here
-	################################################
-	img = Image.fromarray(img.astype("uint8"))
-	rawBytes = io.BytesIO()
-	img.save(rawBytes, "JPEG")
-	rawBytes.seek(0)
-	img_base64 = base64.b64encode(rawBytes.read())
-	return jsonify({'status':str(img_base64)})
+# def mask_image():
+	# # print(request.files , file=sys.stderr)
+	# file = request.files['image'].read() ## byte file
+	# npimg = np.fromstring(file, np.uint8)
+    # #print(npimg.shape)
+	# #img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
+	# ######### Do preprocessing here ################
+	# # img[img > 150] = 0
+	# ## any random stuff do here
+	# ################################################
+	# #img = Image.fromarray(img.astype("uint8"))
+	# #rawBytes = io.BytesIO()
+	# #img.save(rawBytes, "JPEG")
+	# rawBytes.seek(0)
+	# img_base64 = base64.b64encode(rawBytes.read())
+	# return jsonify({'status':str(img_base64)})
 
 
 @app.route('/test' , methods=['GET','POST'])
 def test():
     print("log: got at test" , file=sys.stderr)
+    ResNet50_model = ResNet50(weights='imagenet')
     file = request.files['image'].read()
-    print(file)
+    #npimg = np.fromstring(file, np.uint8)
+    #imgdata = file.split(',')[1]
+    #decoded = base64.b64decode(imgdata)
+    img = Image.open(BytesIO(file))
+    img = img.resize((224,224))
+    x = image.img_to_array(img)
+    print(x.shape)
+    # returns prediction vector for image located at img_path
+    img = rn50_preprocess_input(np.expand_dims(x, axis=0))
+    #print(img.shape)
+    # with graph.as_default():
+        # preds = model.predict(img)
+    prediction = np.argmax(ResNet50_model.predict(img))
+    #print(prediction)
+    #prediction = ResNet50_predict_labels(np.argmax(ResNet50_model.predict(img)))
+    print(((prediction <= 268) & (prediction >= 151)))
+    
+    
+    model_cropped = InceptionResNetV2(weights='imagenet', include_top=False)
+    bottleneck = model_cropped.predict(irnv2_preproccess_input(np.expand_dims(x, axis=0)))
+    doggo_model = Sequential()
+    doggo_model.add(GlobalAveragePooling2D(input_shape=bottleneck.shape[1:]))
+    doggo_model.add(Dense(133))
+    doggo_model.add(Dropout(0.4))
+    doggo_model.add(Dense(133))
+    doggo_model.add(Dropout(0.4))
+    doggo_model.add(Dense(133, activation='softmax'))
+    doggo_model.load_weights('models/weights.best.doggo_model.hdf5')
+    prediction = doggo_model.predict(bottleneck)
+    #race = dog_names[np.argmax(prediction)]
+    print(np.argmax(prediction))
+    
+    
+    
+    #np.expand_dims(x, axis=0)
+    
+    #img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
+    #print(dog_detector(img))
+    #print(file)
     #npimg = np.fromstring(file, np.uint8)
     #print(npimg)
     return jsonify({'status':'succces'})
